f7bfc3effda78c5bda3a5189053b9acf293cd028
MLarson <mlarson@versteel.com>
2025-08-28 13:05:28 -0400

    line item header ints

--- Shipping_Form_CreatorV1/Models/LineItemHeader.cs
+++ Shipping_Form_CreatorV1/Models/LineItemHeader.cs
@@ -1,4 +1,6 @@
-﻿namespace Shipping_Form_CreatorV1.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shipping_Form_CreatorV1.Models;
 
 public class LineItemHeader
 {
@@ -9,5 +11,11 @@ public class LineItemHeader
     public decimal OrderedQuantity { get; set; }
     public decimal PickOrShipQuantity { get; set; }
     public decimal BackOrderQuantity { get; set; }
+    [NotMapped]
+    public int OrderedQuantityInt => (int)OrderedQuantity;
+    [NotMapped]
+    public int PickOrShipQuantityInt => (int)PickOrShipQuantity;
+    [NotMapped]
+    public int BackOrderQuantityInt => (int)BackOrderQuantity;
 }
 
---
+++ Shipping_Form_CreatorV1Tests/Utilities/ReportComparerTests.cs
@@ -0,0 +1,36 @@
+﻿using Shipping_Form_CreatorV1.Models;
+using Shipping_Form_CreatorV1.Utilities;
+
+namespace Shipping_Form_CreatorV1Tests.Utilities;
+
+[TestClass]
+public class ReportComparerTests
+{
+    [TestMethod]
+    public void EquivalentReports_ShouldBeEqual()
+    {
+        var r1 = new ReportModel
+        {
+            Header = new ReportHeader { OrderNumber = 123, ShipToName = "Alice" }
+        };
+        var r2 = new ReportModel
+        {
+            Header = new ReportHeader { OrderNumber = 123, ShipToName = " Alice " } 
+        };
+
+        var equal = ReportComparer.AreEquivalent(r1, r2);
+
+        Assert.IsTrue(equal);
+    }
+
+    [TestMethod]
+    public void DifferentReports_ShouldNotBeEqual()
+    {
+        var r1 = new ReportModel { Header = new ReportHeader { OrderNumber = 123 } };
+        var r2 = new ReportModel { Header = new ReportHeader { OrderNumber = 999 } };
+
+        var equal = ReportComparer.AreEquivalent(r1, r2);
+
+        Assert.IsFalse(equal);
+    }
+}
