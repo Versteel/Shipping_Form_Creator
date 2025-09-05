b4309c81805f1f4b67eebb97aca6b529246123cd
MLarson <mlarson@versteel.com>
2025-09-05 14:30:45 -0400

    search by ship date

---
+++ Shipping_Form_CreatorV1/Components/SearchByDateResultsPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Shipping_Form_CreatorV1.Components;
+
+public partial class SearchByDateResultsPage : Page
+{
+    public SearchByDateResultsPage()
+    {
+        InitializeComponent();
+    }
+}
